f8037b09a29b92c3ee7f33fd240076f89abdf8c7
junyoung.kim <48875061+wnsdudSoftkim@users.noreply.github.com>
2021-07-05 16:53:53 +0900

    ADD(First commit) : Express.js

---
+++ index.js
@@ -0,0 +1,9 @@
+const express = require('express')
+const app = express()
+app.get('/',(req,res)=> {
+    res.send('express.js로 만듬 server입니다.')
+})
+app.listen(3000,()=> {
+    console.log('3000번 port에 http server를 띄웠습니다.')
+
+})
